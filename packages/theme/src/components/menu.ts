65b00601d114145929c905a02a49521c484a3784
kleo <s-kxhindoli@subsplash.com>
2020-09-27 15:45:17 +0200

    feat: menu styles

--- packages/theme/src/components/menu.ts
+++ packages/theme/src/components/menu.ts
@@ -5,10 +5,10 @@ const parts = ["item", "command", "list", "button", "groupTitle"]
 function baseStyleList(props: Record<string, any>) {
   return {
     bg: mode(`#fff`, `gray.700`)(props),
-    boxShadow: mode(`sm`, `dark-lg`)(props),
+    boxShadow: mode(`lg`, `dark-lg`)(props),
     color: "inherit",
     minW: "3xs",
-    py: "2",
+    py: "1",
     zIndex: 1,
     borderRadius: "md",
     borderWidth: "1px",
@@ -17,17 +17,22 @@ function baseStyleList(props: Record<string, any>) {
 
 function baseStyleItem(props: Record<string, any>) {
   return {
-    py: "0.4rem",
-    px: "0.8rem",
+    py: "2",
+    px: "4",
+    fontSize: "sm",
     transition: "background 50ms ease-in 0s",
+    color: mode(`gray.700`, `gray.300`)(props),
     _focus: {
       bg: mode(`gray.100`, `whiteAlpha.100`)(props),
+      color: mode(`gray.900`, `gray.100`)(props),
     },
     _active: {
       bg: mode(`gray.200`, `whiteAlpha.200`)(props),
+      color: mode(`gray.900`, `gray.100`)(props),
     },
     _expanded: {
       bg: mode(`gray.100`, `whiteAlpha.100`)(props),
+      color: mode(`gray.900`, `gray.100`)(props),
     },
     _disabled: {
       opacity: 0.4,
@@ -36,12 +41,13 @@ function baseStyleItem(props: Record<string, any>) {
   }
 }
 
-const baseStyleGroupTitle = {
+const baseStyleGroupTitle = (props: Record<string, any>) => ({
   mx: 4,
   my: 2,
   fontWeight: "semibold",
   fontSize: "sm",
-}
+  color: mode(`gray.500`, `gray.400`)(props),
+})
 
 const baseStyleCommand = {
   opacity: 0.6,
@@ -51,7 +57,7 @@ const baseStyle = function (props: Record<string, any>) {
   return {
     list: baseStyleList(props),
     item: baseStyleItem(props),
-    groupTitle: baseStyleGroupTitle,
+    groupTitle: baseStyleGroupTitle(props),
     command: baseStyleCommand,
   }
 }
